b346ba6ee56f1b86939f471cc0d31719d35fd790
J-Hawk5 <davesspe@urbandaleschools.com>
2019-01-09 12:55:50 -0600

    Input/Output

---
+++ Hello_World.py
@@ -0,0 +1,24 @@
+print("Hello World!")
+print("2+3")
+print(2+3)
+print("Your new score is", 1030+10)
+print("Your new score is", "1030+10")
+print("Your new score is,", 1030+10)
+print("I want to print a double quote \" for some reason.")
+print("The file is sored in C:\new folder")
+print("The file is stored in C:\\new folder")
+print("May the FORCE")
+print("be with you!")
+print("May the FORCE", end=" ")
+print("be with you!")
+i=0
+print(i, end=" ")
+i=1
+print(i, end=" ")
+# This is a comment, it begins with a # sign
+# and the compute will ignore it.
+print("This is not a comment, the computer will")
+print("run this and print it out.")
+print("A # sign between quotes is not a comment.")
+# print("This is a comment even if it is code.")
+print("Hi") # This is an end-of-line comment
